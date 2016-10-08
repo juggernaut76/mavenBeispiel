38a1f33ec82df5ec666b4398b066b2c174562d67
Jasminko Gogic <gogic@hm.edu>
2016-10-08 15:56:40 +0200

    Initial commit

---
+++ src/main/java/com/gogic/beispiel/App.java
@@ -0,0 +1,13 @@
+package com.gogic.beispiel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
