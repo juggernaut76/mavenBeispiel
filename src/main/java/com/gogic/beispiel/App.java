c0fab253685d13faed1b1721a039e5d29347196e
Jasminko Gogic <gogic@hm.edu>
2016-10-15 21:45:24 +0200

    Pmd plugin added

--- src/main/java/com/gogic/beispiel/App.java
+++ src/main/java/com/gogic/beispiel/App.java
@@ -2,11 +2,9 @@ package com.gogic.beispiel;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
